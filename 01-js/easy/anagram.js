b1a7b6de30b6d9b6a9e8ebc8133043d8ec3cd56f
hemanth-92 <hemanth9679@gmail.com>
2023-12-21 08:13:54 +0530

    week-1

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
-
+  function isAnagram(str1, str2) {
+    const cleanStr1 = str1.toUpperCase().split("").sort().join("");
+    const cleanStr2 = str2.toUpperCase().split("").sort().join("");
+    if(cleanStr1 === cleanStr2){
+      return true
+    }
+    return false
+  }
 }
 
 module.exports = isAnagram;
